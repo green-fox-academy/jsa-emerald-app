171ca1e20f8212fea5c731aa61b09834df11ae9d
Michael Chen <tonghaoch@qq.com>
2019-12-04 16:01:31 +0800

    JSAEM2-12 style: refactor the folder structure (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { registerRootComponent } from 'expo';
 import { Provider } from 'react-redux';
 
 import store from './redux/store';
@@ -13,12 +14,12 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function App() {
-  return (
-    <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    </Provider>
-  );
-}
+const App = () => (
+  <Provider store={store}>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  </Provider>
+);
+
+registerRootComponent(App);
