3c68817b68d55560b83816762d0390044897ed6c
KatyTao <katyt3253@gmail.com>
2019-12-06 11:24:21 +0800

    JSAEM2-3 format display color group

--- src/Common/Color.jsx
+++ src/Common/Color.jsx
@@ -1,4 +1,6 @@
 export default {
-  activeTintColor: '#5C6BC0',
-  inactiveTintColor: 'gray',
+  navColor: {
+    activeTintColor: '#5C6BC0',
+    inactiveTintColor: 'gray',
+  },
 };
--- src/Navigator/index.jsx
+++ src/Navigator/index.jsx
@@ -23,7 +23,7 @@ const AppNavigator = createBottomTabNavigator({
     ),
 
   }),
-  tabBarOptions: themeColor,
+  tabBarOptions: themeColor.navColor,
 });
 
 export default AppNavigator;
