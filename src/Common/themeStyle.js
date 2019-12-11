e3e30147d7948ff5257f03aef7046f74c1949f14
Wen Zaiquan <wenzaiquan1995@gmail.com>
2019-12-11 16:08:35 +0800

    JSAEM-2-5_display records finished basic record display page with css. (#7)
    * JSAEM-2-5_display records finished basic record display page with css.
    
    * JSAEM-2_5,modify file structure, add new file
    
    * JSAEM-2_5,remove hard code and modify function with meaningful name
    
    * JSAEM-2_5,add icom to the display page and modify the file name.
    
    * JSAEM-2_5,modify the export and import for ultils.js
    
    * JSAEM-2_5,revert neccessary package

---
+++ src/Common/themeStyle.js
@@ -0,0 +1,73 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  deviceHead: {
+    height: 120,
+    justifyContent: 'flex-end',
+    backgroundColor: 'white',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+
+  deviceBody: {
+    flexDirection: 'column',
+    backgroundColor: '#f0f0f0',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 3,
+    shadowColor: 'grey',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 4.65,
+    elevation: 7,
+    padding: 10,
+    marginTop: 20,
+  },
+
+  cardAlign: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  cardHeader: {
+    color: '#777777',
+    fontSize: 18,
+    margin: 10,
+  },
+
+  redLargeBtn: {
+    marginTop: 20,
+    shadowColor: '#f38aa9',
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.70,
+    shadowRadius: 4.65,
+    elevation: 8,
+  },
+
+  greenLargeBtn: {
+    marginTop: 20,
+    shadowColor: '#70ea93',
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.70,
+    shadowRadius: 4.65,
+    elevation: 8,
+  },
+});
+
+export default styles;
--- src/Stats/index.jsx
+++ src/Stats/index.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, ScrollView } from 'react-native';
+import { ListItem } from 'react-native-elements';
+import { useSelector } from 'react-redux';
+import moment from 'moment';
+import utils from './utils';
+import styles from '../Common/themeStyle';
 
 export default function Stats() {
+  const { transactions } = useSelector((state) => state.transactions);
   return (
-    <View>
-      <Text>Stats Screen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.card}>
+        {utils.sortDataByDate(transactions).map((item, index) => (
+          <ListItem
+            key={`ListKey${index + 1}`}
+            title={item.label ? item.label : 'Undefined'}
+            subtitle={moment.unix(item.date).format('MM/DD/YYYY')}
+            rightTitle={
+              item.type === 'Income'
+                ? ` + $${item.amount}`
+                : ` - $${item.amount}`
+            }
+            leftIcon={{
+              title: 'Unknown',
+              name: 'attach-money',
+            }}
+          />
+        ))}
+      </View>
+    </ScrollView>
   );
 }
