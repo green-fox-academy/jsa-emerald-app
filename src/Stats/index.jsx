7a70e9f73245a9125073be22e6486ddf8a477aec
Wen Zaiquan <wenzaiquan1995@gmail.com>
2019-12-11 13:58:50 +0800

    JSAEM-2_5,add icom to the display page and modify the file name.

--- src/Stats/index.jsx
+++ src/Stats/index.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
-import {
-  Container, Header, Content, View, Text,
-} from 'native-base';
+import { View, ScrollView } from 'react-native';
+import { ListItem } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import moment from 'moment';
-import styles from './style';
-import sortDataByDate from './sortByDateFunction';
+import sortDataByDate from './utils';
+import styles from '../Common/themeStyle';
 
 export default function Stats() {
   const { transactions } = useSelector((state) => state.transactions);
   return (
-    <Container>
-      <Header />
-      <Content>
+    <ScrollView>
+      <View style={styles.card}>
         {sortDataByDate(transactions).map((item, index) => (
-          <View key={`itemID:${index + 1}`} style={styles.inCardBoard}>
-            <View style={{ flex: 2 }}>
-              <Text>
-                {moment.unix(item.date).format('MM/DD/YYYY')}
-              </Text>
-            </View>
-            <View style={{ flex: 1 }}>
-              <Text style={item.type === 'Income' ? styles.inColor : styles.outColor}>
-                {item.type === 'Income' ? `$ +${item.amount}` : `$ -${item.amount}`}
-              </Text>
-            </View>
-          </View>
+          <ListItem
+            key={`ListKey${index + 1}`}
+            title={item.label ? item.label : 'Undefined'}
+            subtitle={moment.unix(item.date).format('MM/DD/YYYY')}
+            rightTitle={
+              item.type === 'Income'
+                ? ` + $${item.amount}`
+                : ` - $${item.amount}`
+            }
+            leftIcon={{
+              title: 'Unknown',
+              name: 'attach-money',
+            }}
+          />
         ))}
-      </Content>
-    </Container>
+      </View>
+    </ScrollView>
   );
 }
