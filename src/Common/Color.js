8cc96fe3b871a9afb2e600d99ea47687b80ef835
Michael Chen <tonghaoch@qq.com>
2019-12-12 09:37:32 +0800

    JSAEM2-6 fixed the merge error

---
+++ src/Common/Color.js
@@ -0,0 +1,14 @@
+export default {
+  navColor: {
+    activeTintColor: '#5C6BC0',
+    inactiveTintColor: 'gray',
+  },
+
+  gradientColor: {
+    blue: ['#9e87fc', '#82c3fb'],
+    red: ['#e96d9e', '#ffaa8f'],
+    green: ['#2fc899', '#78ef93'],
+    purple: ['#a765cf', '#f8b6cc'],
+    orange: ['#ef6d6b', '#feaa74'],
+  },
+};
