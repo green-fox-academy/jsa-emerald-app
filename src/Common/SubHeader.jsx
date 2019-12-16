5a2d28dc9238c954900f8acf66d0368f58b03e8f
Katy <katyt3253@gmail.com>
2019-12-16 17:24:52 +0800

    update filename

---
+++ src/Common/SubHeader.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+import { Button } from 'react-native-elements';
+
+export default function SubHeader(props) {
+  const { viewSet, onPressBtn, viewType } = props;
+  const format = viewType === 'month' ? 'MMM YYYY' : 'YYYY';
+  return (
+    <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+      <Button
+        containerStyle={{ flex: 1, borderBottomWidth: 2, borderBottomColor: 'white' }}
+        title={viewSet[0].format(format)}
+        type="clear"
+        titleStyle={{ color: 'grey' }}
+        onPress={() => onPressBtn(viewSet[0], viewType)}
+      />
+      <View style={{
+        flex: 1, borderBottomWidth: 2, borderBottomColor: '#30d29d', justifyContent: 'center',
+      }}
+      >
+        <Text style={{ textAlign: 'center', fontSize: 18, fontWeight: '500' }}>{viewSet[1].format(format)}</Text>
+      </View>
+
+      <Button
+        containerStyle={{ flex: 1, borderBottomWidth: 2, borderBottomColor: 'white' }}
+        title={viewSet[2].format(format)}
+        type="clear"
+        titleStyle={{ color: 'grey' }}
+        onPress={() => onPressBtn(viewSet[2], viewType)}
+      />
+    </View>
+  );
+}
+
+SubHeader.propTypes = {
+  viewSet: PropTypes.instanceOf(Array),
+  onPressBtn: PropTypes.func,
+  viewType: PropTypes.string,
+};
+
+SubHeader.defaultProps = {
+  viewSet: [],
+  onPressBtn: {},
+  viewType: 'month',
+};
