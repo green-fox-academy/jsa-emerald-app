a65d0c7a84861d30d262a105d3bf6836bd49d2f9
Michael Chen <tonghaoch@qq.com>
2019-12-17 15:07:56 +0800

    JSAEM 2-28 feat: add basic snapshot test

---
+++ src/Navigator/__test__/TabBarIcon.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import TabBarIcon from '../TabBarIcon';
+
+describe('<TabBarIcon />',() => {
+  it('render', () => {
+    const tree = renderer.create(<TabBarIcon
+      navigation={{ state: { routeName: 'Stats' } }}
+      focused
+      tintColor="#5C6BC0"
+    />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
