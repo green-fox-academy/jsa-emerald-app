f37f6a7cd7556fb85e1ca2e07a0b1742c34cfa9a
Michael Chen <tonghaoch@qq.com>
2019-12-17 15:52:56 +0800

    JSAEM2-27 fixed: bug in date picker (#11)
    * JSAEM2-27 fixed: bug in date picker
    
    * JSAEM 2-27 style: rename states' names

--- src/TransCreator/DateSelector.jsx
+++ src/TransCreator/DateSelector.jsx
@@ -1,37 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MaterialIcons } from '@expo/vector-icons';
-import {
-  Text,
-  DatePicker,
-} from 'native-base';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import DateTimePicker from 'react-native-modal-datetime-picker';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import styles from './Style';
 
-
-const DateSelector = ({ transDate, setTransDate }) => (
-  <>
-    <MaterialIcons name="date-range" style={styles.dateItemIcon} />
-    <Text>Date: </Text>
-    <View style={styles.dateView}>
-      <DatePicker
-        defaultDate={new Date()}
-        minimumDate={new Date(1990, 1, 1)}
-        maximumDate={new Date(2050, 12, 31)}
-        locale="en"
-        modalTransparent={false}
-        animationType="fade"
-        androidMode="default"
-        placeHolderText={moment.unix(transDate).format('MM/DD/YYYY')}
-        textStyle={{ color: '#5C6BC0' }}
-        placeHolderTextStyle={{ color: '#5C6BC0' }}
-        onDateChange={(newDate) => setTransDate(moment(newDate).unix())}
-        disabled={false}
-      />
-    </View>
-  </>
-);
+const DateSelector = ({ transDate, setTransDate }) => {
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  return (
+    <>
+      <MaterialIcons name="date-range" style={styles.dateItemIcon} />
+      <Text>Date: </Text>
+      <View style={styles.dateView}>
+        <Text
+          onPress={() => setDatePickerVisibility(true)}
+          style={{ padding: 5 }}
+        >
+          {moment.unix(transDate).format('MM/DD/YYYY')}
+        </Text>
+        <DateTimePicker
+          isVisible={isDatePickerVisible}
+          mode="date"
+          date={new Date(moment.unix(transDate).format('YYYY-MM-DD'))}
+          onCancel={() => setDatePickerVisibility(false)}
+          onConfirm={(date) => {
+            setTransDate(moment(date).unix());
+            setDatePickerVisibility(false);
+          }}
+          titleStyle={{ color: '#5C6BC0' }}
+        />
+      </View>
+    </>
+  );
+};
 
 DateSelector.propTypes = {
   transDate: PropTypes.number,
