2ca1ff73f4c954e3f454ddc64ccd388a4789ed85
Katy <katyt3253@gmail.com>
2019-12-18 13:57:18 +0800

    JSAEM2-15 fixed: fix a typo

--- src/Stats/EmptyTrans.jsx
+++ src/Stats/EmptyTrans.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 export default function EmptyTrans() {
-  return (<View><Text>You haven&quot;t added any transaction yet</Text></View>);
+  return (<View><Text>You haven&quot;t added any transactions yet</Text></View>);
 }
