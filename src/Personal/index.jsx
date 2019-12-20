68f2528ae1dcf2cba69247fcae30179cfaf3dae2
Katy <katyt3253@gmail.com>
2019-12-20 14:33:33 +0800

    JSAEM2-10 fixed: fix lint error

--- src/Personal/index.jsx
+++ src/Personal/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
 import { Avatar, ListItem, Button } from 'react-native-elements';
 import { useDispatch, useSelector } from 'react-redux';
