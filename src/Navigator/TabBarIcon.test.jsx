5f5655ab41e942903442863a770b56a2f78a0cda
Katy <katyt3253@gmail.com>
2019-12-24 09:44:48 +0800

    JSAEM2-30 update: update TabBarIcon test

--- src/Navigator/TabBarIcon.test.jsx
+++ src/Navigator/TabBarIcon.test.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import * as Font from 'expo-font';
 import renderer from 'react-test-renderer';
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons,MaterialIcons} from '@expo/vector-icons';
 
 import TabBarIcon from './TabBarIcon';
 
-beforeAll(() => Font.loadAsync(MaterialIcons.font));
-
 describe('<TabBarIcon />', () => {
-  it('render', () => {
+  beforeEach(()=> Font.loadAsync(MaterialIcons.font));
+  it('render Stats', () => {
     const tree = renderer.create(<TabBarIcon
       navigation={{ state: { routeName: 'Stats' } }}
       focused
@@ -16,4 +15,25 @@ describe('<TabBarIcon />', () => {
     />).toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+  beforeEach(()=> Font.loadAsync(MaterialCommunityIcons.font));
+  it('render Create', ()=>{
+    const tree = renderer.create(<TabBarIcon
+      navigation={{ state: { routeName: 'Create' } }}
+      focused
+      tintColor="#5C6BC0"
+    />).toJSON();
+  expect(tree).toMatchSnapshot();
+  })
+
+  beforeEach(()=> Font.loadAsync(MaterialIcons.font));
+  it('render Me', () => {
+    const tree = renderer.create(<TabBarIcon
+      navigation={{ state: { routeName: 'Me' } }}
+      focused
+      tintColor="#5C6BC0"
+    />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
+
