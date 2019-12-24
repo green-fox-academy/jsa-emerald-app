b96a16b5eed27fd7b3d60ab3096fd78530a7e528
Katy <katyt3253@gmail.com>
2019-12-24 10:03:24 +0800

    JSAEM2-30 fix: fix lint error

--- src/Navigator/TabBarIcon.test.jsx
+++ src/Navigator/TabBarIcon.test.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import * as Font from 'expo-font';
 import renderer from 'react-test-renderer';
-import { MaterialCommunityIcons,MaterialIcons} from '@expo/vector-icons';
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 
 import TabBarIcon from './TabBarIcon';
 
 describe('<TabBarIcon />', () => {
-  beforeEach(()=> Font.loadAsync(MaterialIcons.font));
+  beforeEach(() => Font.loadAsync(MaterialIcons.font));
   it('render Stats', () => {
     const tree = renderer.create(<TabBarIcon
       navigation={{ state: { routeName: 'Stats' } }}
@@ -16,17 +16,17 @@ describe('<TabBarIcon />', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  beforeEach(()=> Font.loadAsync(MaterialCommunityIcons.font));
-  it('render Create', ()=>{
+  beforeEach(() => Font.loadAsync(MaterialCommunityIcons.font));
+  it('render Create', () => {
     const tree = renderer.create(<TabBarIcon
       navigation={{ state: { routeName: 'Create' } }}
       focused
       tintColor="#5C6BC0"
     />).toJSON();
-  expect(tree).toMatchSnapshot();
-  })
+    expect(tree).toMatchSnapshot();
+  });
 
-  beforeEach(()=> Font.loadAsync(MaterialIcons.font));
+  beforeEach(() => Font.loadAsync(MaterialIcons.font));
   it('render Me', () => {
     const tree = renderer.create(<TabBarIcon
       navigation={{ state: { routeName: 'Me' } }}
@@ -36,4 +36,3 @@ describe('<TabBarIcon />', () => {
     expect(tree).toMatchSnapshot();
   });
 });
-
