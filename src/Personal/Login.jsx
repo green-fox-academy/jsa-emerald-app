88ca2764b17980a8dde98e8e84bcbcc6872b78a1
Wen Zaiquan <wenzaiquan1995@gmail.com>
2019-12-25 15:32:53 +0800

    JSAEM2-7,basic form.

---
+++ src/Personal/Login.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import {
+  View, Text, TextInput, TouchableOpacity,
+} from 'react-native';
+import utils from './utils';
+import styles from './LoginView';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handlePasswordChange = (value) => {
+    setPassword(value);
+  };
+  const handleEmailChange = (value) => {
+    setEmail(value);
+  };
+
+  return (
+    <View style={styles.registerForm}>
+      <Text styles={styles.header}>Login</Text>
+
+      <TextInput
+        style={styles.inputText}
+        placeholder="Your email"
+        underlineColorAndroid="transparent"
+        textContentType="emailAddress"
+        value={email}
+        onChangeText={(value) => handleEmailChange(value)}
+      />
+      <Text style={styles.Note}>
+        {email === 'test@1.com' ? 'The email is registered.' : '' }
+        {utils.emailValidation(email) || email === '' ? '' : 'Your email is not correct.' }
+      </Text>
+
+      <TextInput
+        style={styles.inputText}
+        placeholder="Your password"
+        underlineColorAndroid="transparent"
+        secureTextEntry
+        value={password}
+        onChangeText={(value) => handlePasswordChange(value)}
+      />
+      <Text style={styles.Note}>
+        {utils.passwordValidation(password) || password === '' ? '' : 'Password should be 8 to 16 characters.'}
+      </Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>
+          Sign up
+        </Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
---
+++ src/Personal/utils.js
@@ -0,0 +1,14 @@
+function emailValidation(email) {
+  const validateEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return validateEmail.test(String(email).toLowerCase());
+}
+
+function passwordValidation(password) {
+  const validatePassword = /^[0-9a-zA-Z]{8,16}$/;
+  return validatePassword.test(password);
+}
+
+export default {
+  emailValidation,
+  passwordValidation,
+};
