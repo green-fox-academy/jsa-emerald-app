e67271fef22f9bc4b6fc1617d445e0d4966c5b9e
Wen Zaiquan <wenzaiquan1995@gmail.com>
2019-12-27 14:45:58 +0800

    JSAEM2-26, use dotenv , change password validation  and finished redux

--- src/Personal/Login.jsx
+++ src/Personal/Login.jsx
@@ -1,18 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View, Text, TextInput, Button,
 } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigation } from 'react-navigation-hooks';
 import utils from './utils';
 import LoginView from './loginView';
+import { requestLogin } from './actionCreator';
 
 export default function Login() {
+  const { navigate } = useNavigation();
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const login = (userInfo) => {
+    dispatch(requestLogin(userInfo.email, userInfo.password));
+  };
+
+  useEffect(() => {
+    if (user.accessToken) navigate('Index');
+  }, [user.accessToken]);
 
   return (
     <View style={LoginView.registerForm}>
       <Text styles={LoginView.header}>Login</Text>
-
       <TextInput
         style={LoginView.inputText}
         placeholder="Your email"
@@ -24,7 +38,6 @@ export default function Login() {
       <Text style={LoginView.Note}>
         {utils.emailValidation(email) || email === '' ? '' : 'Your email is not correct.' }
       </Text>
-
       <TextInput
         style={LoginView.inputText}
         placeholder="Your password"
@@ -34,14 +47,14 @@ export default function Login() {
         onChangeText={(value) => setPassword(value)}
       />
       <Text style={LoginView.Note}>
-        {utils.passwordValidation(password) || password === '' ? '' : 'Password should be 8 to 16 characters with at least one special characters.'}
+        {utils.passwordValidation(password) || password === '' ? '' : 'Password should be 8 to 16 characters.'}
       </Text>
-
       {
       utils.loginValidation(email, password) ? (
         <Button
           title="Sign Up"
           color="green"
+          onPress={() => login({ email, password })}
         />
       ) : (
         <Button
