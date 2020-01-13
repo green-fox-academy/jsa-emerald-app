3a981528a44f6a251e77da117f48d2925cb79457
Michael Chen <tonghaoch@qq.com>
2020-01-13 15:15:57 +0800

    feat: open banking transaction pull finished

--- src/Personal/openBanking/actionCreator.js
+++ src/Personal/openBanking/actionCreator.js
@@ -1,10 +1,35 @@
+const basicToken = 'Basic ZTlhYzQ1YmItZGQ4Mi00NWEwLWEwOTItYzVhMWJmMzk4NTgxOmRjNmYwNzU3LTZmZDktNGFkOS1hY2M2LTI4NzgyZTI3YjNjNQ==';
+
 export const actionType = {
   SET_BANK_LIST: 'SET_BANK_LIST',
-
+  SET_BANK_ID_CODE: 'SET_BANK_ID_CODE',
+  SET_BANK_ACCESS_TOKEN: 'SET_BANK_ACCESS_TOKEN',
+  SET_ACCOUNT_LIST: 'SET_ACCOUNT_LIST',
+  SET_ACCOUNT_TRANSACTION: 'SET_ACCOUNT_TRANSACTION',
 };
 
-export const SET_BANK_LIST = (list) => ({
+export const setBankList = (list) => ({
   type: actionType.SET_BANK_LIST,
+  payload: list,
+});
+
+export const setBankIDCode = (id, code) => ({
+  type: actionType.SET_BANK_ID_CODE,
+  payload: { id, code },
+});
+
+export const setAccountAccessToken = (token) => ({
+  type: actionType.SET_BANK_ACCESS_TOKEN,
+  payload: token,
+});
+
+export const setAccountList = (list) => ({
+  type: actionType.SET_ACCOUNT_LIST,
+  payload: list,
+});
+
+export const setTransactions = (list) => ({
+  type: actionType.SET_ACCOUNT_TRANSACTION,
   payload: list,
 });
 
@@ -16,5 +41,56 @@ export const loadBankList = () => (dispatch) => {
     },
   })
     .then((response) => response.json())
-    .then((response) => { dispatch(SET_BANK_LIST(response.data)); });
+    .then((response) => { dispatch(setBankList(response.data)); });
+};
+
+const encodeFormData = (data) => Object.keys(data)
+  .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+  .join('&');
+
+export const loadTransactions = (token, id, accountID) => (dispatch) => {
+  console.log(accountID);
+  fetch(`https://api-sandbox.fintecture.com/ais/v1/customer/${id}/accounts/${accountID}/transactions`, {
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => dispatch(setTransactions(response.data)));
+};
+
+export const loadAccountList = (token, account) => (dispatch) => {
+  fetch(`https://oauth-sandbox.fintecture.com/ais/v1/customer/${account}/accounts/`, {
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      dispatch(setAccountList(response.data));
+      dispatch(loadTransactions(token, account, response.data[0].id));
+    });
+};
+
+export const getAccessToken = (code, id) => (dispatch) => {
+  fetch('https://oauth-sandbox.fintecture.com/oauth/accesstoken', {
+    method: 'post',
+    headers: {
+      Accept: 'application/json',
+      Authorization: basicToken,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: encodeFormData({
+      grant_type: 'authorization_code',
+      code,
+      scope: 'AIS',
+    }),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      dispatch(setAccountAccessToken(response.access_token));
+      dispatch(loadAccountList(response.access_token, id));
+    });
 };
--- src/Personal/openBanking/obReducer.js
+++ src/Personal/openBanking/obReducer.js
@@ -2,6 +2,9 @@ import { actionType } from './actionCreator';
 
 const initialOB = {
   bankList: [],
+  accountID: null,
+  accountCode: null,
+  accessToken: null,
   accountList: [],
   transactions: [],
 };
@@ -9,7 +12,31 @@ const initialOB = {
 const backupState = (state = initialOB, action) => {
   switch (action.type) {
     case actionType.SET_BANK_LIST:
-      return { ...state, bankList: action.payload };
+      return {
+        ...state,
+        bankList: action.payload,
+      };
+    case actionType.SET_BANK_ID_CODE:
+      return {
+        ...state,
+        accountID: action.payload.id,
+        accountCode: action.payload.code,
+      };
+    case actionType.SET_BANK_ACCESS_TOKEN:
+      return {
+        ...state,
+        accessToken: action.payload,
+      };
+    case actionType.SET_ACCOUNT_LIST:
+      return {
+        ...state,
+        accountList: action.payload,
+      };
+    case actionType.SET_ACCOUNT_TRANSACTION:
+      return {
+        ...state,
+        transactions: action.payload,
+      };
     default:
       return state;
   }
