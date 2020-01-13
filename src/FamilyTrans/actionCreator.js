90929e211ecb25931cb648f20b3d3d74a46e58aa
Katy <katyt3253@gmail.com>
2020-01-13 16:40:31 +0800

    update

--- src/FamilyTrans/actionCreator.js
+++ src/FamilyTrans/actionCreator.js
@@ -1,19 +1,10 @@
 import { BACKEND_URL, UPDATE_URL } from 'react-native-dotenv';
 
 export const actionType = {
-  // ADD_FAMILY_TRANS: 'ADD_FAMILY_TRANS',
   ADD_FAMILY_TRANS_FAILED: 'ADD_FAMILY_TRANS_FAILED',
   GET_FAMILY_TRANS: 'GET_FAMILY_TRANS',
+  GET_FAMILY_TRANS_FAILED: 'GET_FAMILY_TRANS_FAILED',
 };
-
-// export function addFamilyTrans(type, date, amount, label) {
-//   return {
-//     type: actionType.ADD_FAMILY_TRANS,
-//     data: {
-//       type, date, amount, label,
-//     },
-//   };
-// }
 
 export function addFamilyTransFailed(error) {
   return {
@@ -26,6 +17,13 @@ export function getFamilyTrans(dataSet) {
   return {
     type: actionType.RESTORE_TRANS,
     dataSet,
+  };
+}
+
+export function getFamilyTransFailed(error) {
+  return {
+    type: actionType.GET_FAMILY_TRANS_FAILED,
+    error,
   };
 }
 
@@ -56,3 +54,11 @@ export const addFamilyTransactions = ({ transaction }) => (dispatch, getState) =
     dispatch(addFamilyTransFailed('Network error, please try again later'));
   });
 };
+
+export const getFamilyTransactions = () => (dispatch, getState) => {
+  const { accessToken } = getState().user;
+  if (!accessToken) {
+    dispatch(getFamilyTransFailed('Unauthorized token'));
+    return;
+  }
+}
