f0107f8fdbe87f37c570ac1c7cca6d3575670256
Michael Chen <tonghaoch@qq.com>
2020-01-15 09:11:56 +0800

    feat: add animation for trans list

--- src/Stats/TransList.jsx
+++ src/Stats/TransList.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  View, Text, Animated,
+} from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import GradientIcon from '../Common/GradientIcon';
@@ -10,37 +12,48 @@ export default function TransList(props) {
   const { transactions } = props;
   const { themeMode } = useSelector((state) => state.theme);
   const styles = setThemeStyle(themeMode);
+  const [fadeAnim] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1200,
+    }).start();
+  }, []);
+
   return (
     transactions.map((groupedRecordsByDate, index) => (
-      <View style={styles.card} key={`DateGroup:${index + 1}`}>
-        <View style={styles.cardAlign}>
-          <Text style={styles.cardHeader}>
-            {groupedRecordsByDate[0].date}
-          </Text>
-          <Text style={styles.cardHeader}>
-            {utils.sumAmount(groupedRecordsByDate)}
-          </Text>
-        </View>
-        {groupedRecordsByDate.map((item, idx) => (
-          <ListItem
-            key={`InfoGroup:${idx + 1}`}
-            leftElement={(
-              <GradientIcon
-                name={item.labelName ? item.labelName.icon : 'home'}
-                color={item.labelName && item.labelName.color ? item.labelName.color : '#9e87fc'}
-                iconFamily={item.labelName && item.labelName.iconFamily ? item.labelName.iconFamily : ''}
-              />
+      <Animated.View style={{ opacity: fadeAnim }} key={`DateGroup:${index + 1}`}>
+        <View style={styles.card}>
+          <View style={styles.cardAlign}>
+            <Text style={styles.cardHeader}>
+              {groupedRecordsByDate[0].date}
+            </Text>
+            <Text style={styles.cardHeader}>
+              {utils.sumAmount(groupedRecordsByDate)}
+            </Text>
+          </View>
+          {groupedRecordsByDate.map((item, idx) => (
+            <ListItem
+              key={`InfoGroup:${idx + 1}`}
+              leftElement={(
+                <GradientIcon
+                  name={item.labelName ? item.labelName.icon : 'home'}
+                  color={item.labelName && item.labelName.color ? item.labelName.color : '#9e87fc'}
+                  iconFamily={item.labelName && item.labelName.iconFamily ? item.labelName.iconFamily : ''}
+                />
             )}
-            title={item.labelName ? item.labelName.name : 'Unknown'}
-            titleStyle={styles.listHeading}
-            subtitle={item.creator ? item.creator : 'Unknown'}
-            subtitleStyle={item.creator ? { color: 'grey' } : { display: 'none' }}
-            rightTitle={utils.transType(item.amount, item.type)}
-            rightTitleStyle={styles.listHeading}
-            containerStyle={{ backgroundColor: 'rgba(255,0, 0, 0)' }}
-          />
-        ))}
-      </View>
+              title={item.labelName ? item.labelName.name : 'Unknown'}
+              titleStyle={styles.listHeading}
+              subtitle={item.creator ? item.creator : 'Unknown'}
+              subtitleStyle={item.creator ? { color: 'grey' } : { display: 'none' }}
+              rightTitle={utils.transType(item.amount, item.type)}
+              rightTitleStyle={styles.listHeading}
+              containerStyle={{ backgroundColor: 'rgba(255,0, 0, 0)' }}
+            />
+          ))}
+        </View>
+      </Animated.View>
     ))
   );
 }
